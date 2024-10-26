53f2e56e38688b13125fc2b7be3cadfe2eae7c95
Om8983 <omwadhi64@gmail.com>
2024-10-27 00:23:20 +0530

    packages uploaded

---
+++ common/src/index.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+// user schemas
+// this is while user is trying to signup or login
+export const userSignUpSchema = z.object({
+  email: z.string().email(),
+  username: z.string(),
+  password: z.string().min(8),
+});
+
+export const userLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+
+// blog schemas 
+export const blogCreateSchema = z.object({
+  title : z.string(),
+  content : z.string()
+});
+
+export const blogUpdateSchema = z.object({
+  blogId : z.string(),
+  title : z.string(),
+  content : z.string()
+});
+
+
+// vsriable that will be used by the frontend while passing data
+export type SignUpSchema = z.infer<typeof userSignUpSchema>;
+export type LoginSchema = z.infer<typeof userLoginSchema>;
+export type BlogCreate = z.infer<typeof blogCreateSchema>;
+export type BlogUpdate = z.infer<typeof blogUpdateSchema>;

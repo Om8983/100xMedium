3ce1f1f5ffafbe47aabae7ed34eaba315979582a
Om8983 <omwadhi64@gmail.com>
2024-10-26 22:39:24 +0530

    package creation complete

---
+++ common/dist/index.d.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+export declare const userSignUpSchema: z.ZodObject<{
+    email: z.ZodString;
+    username: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    username: string;
+    password: string;
+}, {
+    email: string;
+    username: string;
+    password: string;
+}>;
+export declare const userLoginSchema: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare const blogCreateSchema: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export declare const blogUpdateSchema: z.ZodObject<{
+    blogId: z.ZodString;
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    blogId: string;
+}, {
+    title: string;
+    content: string;
+    blogId: string;
+}>;
+export type SignUpSchema = z.infer<typeof userSignUpSchema>;
+export type LoginSchema = z.infer<typeof userLoginSchema>;
+export type BlogCreate = z.infer<typeof blogCreateSchema>;
+export type BlogUpdate = z.infer<typeof blogUpdateSchema>;
---
+++ common/dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.blogUpdateSchema = exports.blogCreateSchema = exports.userLoginSchema = exports.userSignUpSchema = void 0;
+const zod_1 = require("zod");
+// user schemas
+// this is while user is trying to signup or login
+exports.userSignUpSchema = zod_1.z.object({
+    email: zod_1.z.string().email(),
+    username: zod_1.z.string(),
+    password: zod_1.z.string().min(8),
+});
+exports.userLoginSchema = zod_1.z.object({
+    email: zod_1.z.string().email(),
+    password: zod_1.z.string().min(8),
+});
+// blog schemas 
+exports.blogCreateSchema = zod_1.z.object({
+    title: zod_1.z.string(),
+    content: zod_1.z.string()
+});
+exports.blogUpdateSchema = zod_1.z.object({
+    blogId: zod_1.z.string(),
+    title: zod_1.z.string(),
+    content: zod_1.z.string()
+});
